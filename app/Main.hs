c7801aad7ae2eeb494fcfaabcbcaf4f4a40c9627
Tom Heaton <thomas.m.heaton@gmail.com>
2023-02-08 18:25:30 +0000

    factorial

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,15 @@ module Main where
 
 import qualified MyLib (someFunc)
 
+factorial :: Integer -> Integer
+factorial n
+  | n <= 0 = 1
+  | otherwise = n * factorial (n - 1)
+
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   MyLib.someFunc
+
+  let result = factorial 10
+  putStrLn $ "Factorial of 10 is " ++ show result
