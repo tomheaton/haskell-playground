154df55824afe253f917dcc3b507b7cb57b49953
Tom Heaton <thomas.m.heaton@gmail.com>
2023-04-03 23:52:29 +0100

    more trees

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,18 @@ addOne n = n + 1
 -- data Tree = Leaf | Node Tree Tree
 data IntTree = Empty | Node Int IntTree IntTree deriving Show
 
+size :: IntTree -> Int
+size Empty = 0
+size (Node x left right) = size left + size right + 1
+
+total :: IntTree -> Int
+total Empty = 0
+total (Node x left right) = total left + total right + x
+
+treemap :: (Int -> Int) -> IntTree -> IntTree
+treemap f Empty = Empty
+treemap f (Node x left right) = Node (f x) (treemap f left) (treemap f right)
+
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
